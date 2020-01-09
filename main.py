21e44409983992db03e8fd05ab9c4321a751902e
Jasson Mora <jasson.mora@gmail.com>
2020-01-09 22:29:22 +0000

    Push consume

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import time
 import random
 import datetime
 import telepot
+import requests
+import json
 
 def handle(msg):
 	chat_id = msg['chat']['id']
@@ -11,10 +13,9 @@ def handle(msg):
 	print(' from id ', chat_id)
 	print('got msg from ',chat_id)
 	print ('got the msg', command)
-	if chat_id == 968761100:
-		bot.sendMessage(chat_id,'Checho mk siempre llega tarde a todo')
-	else:
-		bot.sendMessage(chat_id, 'Hello, '+user)
+	r = requests.put('http://172.16.16.56:18180/chat', data = {'chatId', chat_id})
+	if chat_id !=  r:
+		bot.sendMessage(chat_id,'Te quieres quieres registrar '+user+ '?')
 bot = telepot.Bot('946350488:AAFCySrUxncEdgPMxgwIVZy4_ho7SSGJgxE')
 bot.message_loop(handle)
 print ('Wait for it!')
